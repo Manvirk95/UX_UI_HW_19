e85cf6df689e73f6c74f26b0770f1a4b3104e5ac
Manvirk95 <Manvirk95@gmail.com>
2022-04-07 00:44:00 -0500

    edits made in about.html, index.html, index.js & main.css
    Added bootstrap components for hover on buttons, back to top animation using javascript.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 //Get the button
 let mybutton = document.getElementById("back2top");
 
-
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
-
 
 function scrollFunction() {
   if (
@@ -19,11 +16,11 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
-
-
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  window.scrollTo({top: 0, behavior: "smooth"})
+  // document.body.scrollTop = 0;
+  // document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
