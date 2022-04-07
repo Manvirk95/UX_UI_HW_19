221cbea1009ea6b94a0205d2d7e3331991796ac7
Manvirk95 <Manvirk95@gmail.com>
2022-04-06 23:35:15 -0500

    edited js/index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 //Get the button
 let mybutton = document.getElementById("back2top");
 
+
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
   scrollFunction();
 };
+
 
 function scrollFunction() {
   if (
@@ -16,10 +19,11 @@ function scrollFunction() {
     mybutton.style.display = "none";
   }
 }
+
+
 // When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
 
 function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: "smooth"})
 }
